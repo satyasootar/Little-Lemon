d8b87979e8fef82e543736ccd4a2b9f7c9f8dec3
satyasootar <satya.sootar06@gmail.com>
2024-10-26 19:45:30 +0530

    added loader

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ function App() {
     <>
       {loading ? (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <InfinitySpin
-  visible={true}
-  width="200"
-  color="#4fa94d"
-  ariaLabel="infinity-spin-loading"
-  />
+          <InfinitySpin
+            visible={true}
+            width="200"
+            color="#4fa94d"
+            ariaLabel="infinity-spin-loading"
+          />
         </div>
       ) : (
         <>
